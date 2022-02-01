17f0bce5b465add37367c5bcea0c9a770e667be8
Damien Milon <37899667+dmilon@users.noreply.github.com>
2022-02-01 12:36:52 +0100

    ruby 2.7.4

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,12 +3,17 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
-require("@rails/ujs").start()
-require("turbolinks").start()
-require("@rails/activestorage").start()
-require("channels")
+import Rails from "@rails/ujs"
+import Turbolinks from "turbolinks"
+import * as ActiveStorage from "@rails/activestorage"
+import "channels"
 
-import 'bootstrap';
+Rails.start()
+Turbolinks.start()
+ActiveStorage.start()
+
+// Bootstrap import
+import "bootstrap";
 import 'select2/dist/css/select2.min.css'
 import 'jquery-bar-rating/dist/themes/css-stars';
 import 'aos/dist/aos.css';
